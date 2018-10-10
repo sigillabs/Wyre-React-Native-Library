429209a08dc2737da060273a62788491cd901a4a
Abraham Elmahrek <abraham@elmahrek.com>
2018-10-10 10:06:38 -0700

    modify for react-native

--- README.md
+++ README.md
@@ -1,34 +1,19 @@
-This is the React.js component for Wyre Verification.
+This is the react-native component for Wyre Verification.
 
 ## Installation
 
-`npm install wyre-react-library`
+`npm install wyre-react-native-library`
 
 ## Usage
 
 import the library
 
 ```
-import WyreVerification from 'wyre-react-library';
+import WyreVerification from 'wyre-react-native-library';
 ```
-
-The network you are using is decided base on metamask through [web3-webpacked-react](https://github.com/NoahHydro/web3-webpacked-react). If you are on mainnet it will go to production where tokens can be viewed [here](https://etherscan.io/address/0x9f2a7b5e6280727cd6c8486f5f96e5f76164f2df), on any testnet it will go to sandbox (currently sends tokens to kovan which can be viewed [here](https://kovan.etherscan.io/address/0xb14fa2276d8bd26713a6d98871b2d63da9eefe6f)
 
 render the component
-```
-<WyreVerification myButton={myButton} apiKey={apiKey}/>
-```
-
-You will need to generate your own button and pass it in as a prop. One example implementation using the Material-Ui Button would be:
 
 ```
-const myButton = OurButton
-
-...then make a function to render it
-
-const OurButton = (props) => {
-  return (
-    <Button {...props} variant="contained" color="primary">Open Wyre</Button>
-  );
-}
+<WyreVerification apiKey={apiKey}/>
 ```
--- src/WyreVerification.js
+++ src/WyreVerification.js
@@ -1,64 +1,49 @@
+import * as PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import Iframe from 'react-iframe';
-import { withWeb3 } from 'web3-webpacked-react';
+import { Text, TouchableOpacity, View, WebView } from 'react-native';
+// import Web3Webview from 'react-native-web3-webview';
 
-const styles = {
-  zIndex: "999999",
-  backgroundColor: "transparent",
-  top: "0",
-  bottom: "0",
-  left: "0",
-  right: "0",
-}
-
-class WyreVerification extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      wyreIframeOpen: false,
-    }
-
-  }
-
+export default class WyreVerification extends Component {
   componentDidMount() {
-    let eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-    let messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
-    window[eventMethod](messageEvent, (e) => {
-        switch (e.data) {
-            case "close":
-                this.setState({wyreIframeOpen: false})
-                break;
-            case "complete":
-                this.setState({wyreIframeOpen: false})
-                break;
-            default:
-        }
-    }, false);
+    // window.web3 = new Web3({
+    //   sendAsync: function(payload, cb) {
+    //     postMessage('here');
+    //   }
+    // });postMessage('here1');
+    const js = `postMessage(typeof window.web3);`;
+    this.webview.injectJavaScript(js);
   }
 
   render() {
-    const MyButton = this.props.myButton;
-    const apiKey  = this.props.apiKey;
-    const url = this.props.w3w.networkId === "1" ? "https://verify.sendwyre.com/?apiKey=" : "https://verify.testwyre.com/?apiKey="
-    const buttonDisplay = !this.state.wyreIframeOpen ? "block" : "none"
-    const display = this.state.wyreIframeOpen ? "block" : "none"
+    const { apiKey, networkId } = this.props;
+    const url =
+      networkId === 1
+        ? 'https://verify.sendwyre.com/?apiKey='
+        : 'https://verify.testwyre.com/?apiKey=';
+    const uri = `${url}${apiKey}`;
 
     return (
-    <div id="wyre-div" style={{height: "570px"}}>
-      <MyButton
-        style={{ display: buttonDisplay }}
-        onClick={() => { this.setState({wyreIframeOpen: true}); }}
+      <WebView
+        {...this.props}
+        ref={ref => (this.webview = ref)}
+        // injectedJavaScript={`postMessage(typeof window.web3);`}
+        onMessage={e => {
+          console.warn(e);
+        }}
+        // onLoadStart={err => console.warn('Start')}
+        // onLoad={err => console.warn('loaded')}
+        // onLoadEnd={err => console.warn('Finished')}
+        // onError={err => console.warn(err)}
+        source={{ uri }}
+        originWhitelist={['*']}
+        useWebKit={true}
       />
-      <Iframe
-        url={url + apiKey}
-        display={display}
-        styles={{styles}}
-        position="relative"
-      />
-    </div>
-  ); }
-
+    );
+  }
 }
 
-export default withWeb3(WyreVerification);
+WyreVerification.propTypes = {
+  networkId: PropTypes.number.isRequired,
+  apiKey: PropTypes.string.isRequired,
+  web3: PropTypes.object.isRequired
+};
